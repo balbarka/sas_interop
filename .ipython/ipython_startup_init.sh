507f70b84f84c6a6f7e6d47bbce3842746b9bbb0
balbarka <balbarka>
2023-08-08 15:31:17 +0000

    ipython startup ref init code

--- .ipython/ipython_startup_init.sh
+++ .ipython/ipython_startup_init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -z ${IPYTHON_DIR+x} ]
+if [ -z ${IPYTHON_PROFILE_DIR+x} ]
 then
-    echo "IPYTHON_DIR is unset"
+    echo "IPYTHON_PROFILE_DIR is unset"
 else
-    echo "IPYTHON_DIR is set to '$IPYTHON_DIR'"
+    echo "IPYTHON_PROFILE_DIR is set to '$IPYTHON_PROFILE_DIR'"
     if [[ $DB_IS_DRIVER = "TRUE" ]]; then
       ipython profile create default
       mkdir -p /root/.ipython/profile_default/startup
       rm -f /root/.ipython/profile_default/startup/*.py
-      cp $IPYTHON_DIR/profile_default/startup/*.py /root/.ipython/profile_default/startup
+      cp $IPYTHON_PROFILE_DIR/startup/*.py /root/.ipython/profile_default/startup
     fi
 fi
